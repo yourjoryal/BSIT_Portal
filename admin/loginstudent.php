5acf0bc31f563a023b5e51a5216d9c6716231f8d
villanuevaclayd82-tech <villanuevaclayd82@gmail.com>
2025-12-04 00:32:59 +0800

    Update loginstudent.php

--- admin/loginstudent.php
+++ admin/loginstudent.php
@@ -2,31 +2,6 @@
     include ("studentdatabase.php");
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Registration</title>
-    <link rel="stylesheet" href="../css/loginstudent.css">
-</head>
-<body>
-
-<h2>Register</h2>
-
-<form action="<?php htmlspecialchars($_SERVER["PHP_SELF"])?>" method="post">
-    <label>Username:</label><br>
-    <input type="text" name="username" required><br><br>
-
-    <label>Password:</label><br>
-    <input type="password" name="password" required><br><br>
-
-    <button type="submit">Register</button>
-</form>
-
-</body>
-</html>
-
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
@@ -52,4 +27,5 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }
 }
     mysqli_close($conn);
+
 ?>
